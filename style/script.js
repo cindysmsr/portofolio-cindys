7208ab2257b212c8a9e076932214c87b90474836
cindy samosir <cindysamosir98@gmail.com>
2024-10-28 20:58:18 +0700

    first commit

---
+++ style/script.js
@@ -0,0 +1,9 @@
+function sayHi() {
+    alert('Hi! How are you?');
+  };
+
+  window.onload = function() {
+    document.getElementById('progress-html').style.width = '80%';
+    document.getElementById('progress-css').style.width = '80%';
+    document.getElementById('progress-bootstrap').style.width = '75%';
+  };
